06ca8f68d1c112f3e778bd0690b814b2be8bfcde
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-11 00:55:45 +0400

    feat: connected users route to app

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const { createUser } = require('./controllers/users');
+const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/signup', createUser);
+app.use('/users', users);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
