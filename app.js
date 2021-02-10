e321b4d11bf6043159aeb1509e81e2d1802eb8f8
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-11 00:56:44 +0400

    feat: added creating db mongo

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const { createUser } = require('./controllers/users');
+const mongoose = require('mongoose');
 const users = require('./routes/users');
+const { createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 
