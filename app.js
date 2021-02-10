4c3073968da9c33b5206fa5f2dd3ff52c286aa7a
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-11 01:01:57 +0400

    feat: changed db name

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { createUser } = require('./controllers/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
