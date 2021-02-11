b850ca49b4fc98843c889e30dcaa995825c4edf1
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-12 03:41:39 +0400

    feat: added error handler

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-// const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const { corsOptions, limiter } = require('./config');
+const { limiter } = require('./config');
 const routes = require('./routes');
+const errorHeandler = require('./middlewares/error-heandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
@@ -22,7 +22,6 @@ mongoose.connect(MONGO_URL, {
 
 app.use(limiter);
 app.use(helmet());
-// app.use(cors(corsOptions));
 app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,16 +29,6 @@ app.use(cookieParser());
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500 ? 'На сервере произошла ошибка =('
-        : message,
-    });
-  next();
-});
+app.use(errorHeandler);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
--- config.js
+++ config.js
@@ -1,30 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
-const whiteList = [
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'http://localhost:3001',
-  'https://localhost:3001',
-];
-
-const corsOptions = {
-  origin(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
   max: 100, // можно совершить максимум 100 запросов с одного IP
 });
 
 module.exports = {
-  corsOptions,
   limiter,
 };
