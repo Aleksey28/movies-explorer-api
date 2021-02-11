bb2209fc5fdd72ecdeba9c6c491dac5830a98b2f
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-11 22:54:25 +0400

    feat: connected cors

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const users = require('./routes/users');
 const { createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+const whiteList = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+];
+
+const corsOption = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
