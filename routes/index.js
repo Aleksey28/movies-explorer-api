3a03bd04cf9bfbe12d760f14244a4fa547484dfa
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-02-13 20:05:21 +0400

    fix: deleted check-password.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 const router = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./users');
 const movieRouter = require('./movies');
 const { createUser, login, logout } = require('../controllers/users');
-const checkPassword = require('../middlewares/check-password');
 const auth = require('../middlewares/auth');
 const NotFoundErr = require('../errors/not-found-err');
 const { validateAuthentication, validateUserCreate } = require('../middlewares/validations');
 
 router.all('/', auth);
-router.post('/signup', validateUserCreate, checkPassword, createUser);
-router.post('/signin', validateAuthentication, checkPassword, login);
+router.post('/signup', validateUserCreate, createUser);
+router.post('/signin', validateAuthentication, login);
 router.get('/signout', logout);
 
 router.use('/users', auth, usersRouter);
